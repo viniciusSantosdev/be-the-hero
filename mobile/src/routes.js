b255d5ff8b5dbe5ce964d8a81f7ce45f1dc467fd
Vinicius Santos <santos.viniciusmt@gmail.com>
2020-03-30 00:19:50 -0300

    front end with React, back end with Nodejs and Mobile with React Native

---
+++ mobile/src/routes.js
@@ -0,0 +1,18 @@
+import {NavegationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Incidents from './pages/Incidents';
+import Detail from '../pages/Detail';
+
+export default function Routes(){
+    return (
+        <NavegationContainer>
+            <AppStack.Navigator screenOptions={{headerShown: false}}>
+                    <AppStack.Screen name="Incidents" component = {Incidents} />
+                    <AppStack.Screen name="Detail" component = {Detail} />
+            </AppStack.Navigator>
+        </NavegationContainer>
+    );
+}
